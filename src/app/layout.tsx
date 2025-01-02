6619b3a59e24b01fbd43d93979466d46da785a1b
O-Isaac <soporte@proyectograndorder.es>
2025-01-02 21:08:47 +0100

    feat: add bg-hero grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ const RootLayout: React.FC<RootLayout> = ({ children }) => (
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-neutral-900 dark:text-neutral-200`}
     >
+      <div className="bg-hero absolute -z-10 inset-0 size-full bg-neutral-200 dark:bg-neutral-900 bg-[linear-gradient(to_right,#80808012_2px,transparent_2px),linear-gradient(to_bottom,#80808012_2px,transparent_2px)] bg-[size:40px_40px]"></div>
       <ThemeProvider attribute="class">
         <Navigation items={[]} />
         <main>{children}</main>

c809df93a82afc904d4c7a4f8c9d8ada7a433912
O-Isaac <soporte@proyectograndorder.es>
2025-01-05 13:34:42 +0100

    feat: new card work

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -9,7 +9,16 @@ const Board: React.FC<Board> = ({ items, title }) => {
       <div className="bg-neutral-100 dark:bg-neutral-800 board-body p-4 rounded-b-lg">
         <ul>
           {items.map((item) => {
-            return <li key={item.event}>{item.event}</li>;
+            return (
+              <li key={item.event}>
+                <article className="w-full p-4 border my-2 rounded-xl bg-neutral-200 dark:bg-neutral-900 dark:border-neutral-800">
+                  <h4>{item.event}</h4>
+                  <p className="bg-green-500 rounded-full px-2 text-black inline">
+                    {item.completed}/{item.lines}
+                  </p>
+                </article>
+              </li>
+            );
           })}
         </ul>
       </div>

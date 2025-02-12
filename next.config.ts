6d8eaff36280e3cf883892f9df12621b286ad4c4
O-Isaac <soporte@proyectograndorder.es>
2025-02-12 21:11:19 +0100

    feat: add remote image patterns for external sources

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "www.proyectograndorder.es"
+      },
+      {
+        hostname: "imgur.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;

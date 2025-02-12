75102d66429edafd9977d554e587d177ddbbc215
O-Isaac <soporte@proyectograndorder.es>
2025-02-12 21:11:47 +0100

    feat: enhance board component with image support and percentage display

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,4 +1,10 @@
 import type { Board } from "@/types/board";
+import Image from "next/image";
+
+const toPercent = (current: number, total: number) => {
+  const percent = (current / total) * 100;
+  return Number.isNaN(percent) ? "0" : percent.toFixed(0);
+};
 
 const Board: React.FC<Board> = ({ items, title }) => {
   return (
@@ -11,11 +17,14 @@ const Board: React.FC<Board> = ({ items, title }) => {
           {items.map((item) => {
             return (
               <li key={item.event}>
-                <article className="w-full p-4 border my-2 rounded-xl bg-neutral-200 dark:bg-neutral-900 dark:border-neutral-800">
-                  <h4>{item.event}</h4>
-                  <p className="bg-green-500 rounded-full px-2 text-black inline">
-                    {item.completed}/{item.lines}
-                  </p>
+                <article className="w-full border my-2 rounded-xl bg-neutral-200 dark:bg-neutral-900 dark:border-neutral-800">
+                  {item.image && <Image className="rounded-xl -z-0 relative fade-image" width={2688} height={1252} src={item.image} alt="Banner" />}
+                  <div className="p-4">
+                    <h4>{item.event}</h4>
+                    <p className="bg-green-500 rounded-full px-2 text-black inline">
+                      {toPercent(item.completed, item.lines)}%
+                    </p>
+                  </div>
                 </article>
               </li>
             );
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,13 +3,14 @@ import type { Navigation } from "@/types/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
-import Documentation from "@/icons/documentation";
 import { ThemeSwitch } from "./dyanmic";
+
+import Documentation from "@/icons/documentation";
 import Github from "@/icons/github";
 
 const Navigation: React.FC<Navigation> = () => {
   return (
-    <header className="h-auto w-full border-b sticky top-0 bg-white/70 dark:bg-neutral-900/70 dark:border-neutral-800 backdrop-blur-md">
+    <header className="h-auto w-full border-b sticky top-0 bg-white/70 dark:bg-neutral-900/70 dark:border-neutral-800 backdrop-blur-md z-50">
       <nav className="flex gap-2 p-3 justify-between items-center container mx-auto">
         <Link className="flex items-center gap-1" href="/">
           <Image
